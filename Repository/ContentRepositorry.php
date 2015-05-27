949fb86eac1d0ad2a52c7ff3dd6315e6a016ebd8
Johnfordman <ndoulou.jean@gmail.com>
2015-05-27 11:42:37 +0200

    Modif class

---
+++ Repository/ContentRepositorry.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fordman
+ * Date: 27/05/15
+ * Time: 11:39
+ */
+
+namespace Repository;
+
+
+class ContentRepository
+{
+    public function add(){
+
+    }
+    public function delete(){
+
+    }
+    public function update(){
+
+    }
+    public function view(){
+
+    }
+    public function liste(){
+
+    }
+}
